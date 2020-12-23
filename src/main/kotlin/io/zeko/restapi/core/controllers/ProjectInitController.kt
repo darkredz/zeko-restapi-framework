a105c41bdbac432a0b94d1e13dc64897afe27f2d
Leng Sheng Hong <darkredz@gmail.com>
2020-12-23 11:43:07 +0800

    update version

--- src/main/kotlin/io/zeko/restapi/core/controllers/ProjectInitController.kt
+++ src/main/kotlin/io/zeko/restapi/core/controllers/ProjectInitController.kt
@@ -374,9 +374,9 @@ mvn clean compile vertx:run -Dvertx.verticle="$packageName.BootstrapVerticle" \
 
     <properties>
         <vertx.verticle>$packageName.BootstrapVerticle</vertx.verticle>
-        <kotlin.version>1.3.61</kotlin.version>
-        <zeko-restapi.version>1.2.6</zeko-restapi.version>
-        <vertx.version>3.9.4</vertx.version>
+        <kotlin.version>1.4.10</kotlin.version>
+        <zeko-restapi.version>1.3.0</zeko-restapi.version>
+        <vertx.version>4.0..0</vertx.version>
         <micrometer.version>1.1.0</micrometer.version>
         <java.version>1.8</java.version>
         <jib.version>2.2.0</jib.version>
